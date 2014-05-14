00c6b634e94023c21aae3163d0ae73b1a7c3daf3
Eddie Johnston <eddiej@Eddies-MacBook-Pro.local>
2014-05-14 17:12:56 +0100

    removed extra view file

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -7,15 +7,15 @@ class CalculationsController < ApplicationController
   end
 
   def create
-    operator = MAPPINGS[params['commit']]
-    a,b = params[:calculation][:a].to_i, params[:calculation][:b].to_i
+    operator = MAPPINGS[params['commit']] # get the string sent with the form submit and lookup the operator from the MAPPINGS hash.
+    a,b = params[:calculation][:a].to_i, params[:calculation][:b].to_i # the operands.
     if ['+','*'].include? operator and b < a then a,b = b,a end # order of operands doesnt matter for sum or product. Sort before saving record to avoid needless calculations being saved.   
-    c = Calculation.find_or_initialize_by(a: a, b: b, operator: operator)
+    c = Calculation.find_or_initialize_by(a: a, b: b, operator: operator) # Find or initialise the Calcualtion object.
     if c.new_record?
-      c.result = eval("#{params[:calculation][:a].to_f} #{operator} #{params[:calculation][:b].to_f}")
+      c.result = eval("#{params[:calculation][:a].to_f} #{operator} #{params[:calculation][:b].to_f}") # Perform the calculation. 
       c.save!
     else   
-      c.update_attributes({:count => c.count+1}) # c.inc(:count,1) doesn't appear to be working with current alpha release of mongoid for Rails 4 (mongoid 4.0.0.alpha1) so using slightly longer method below
+      c.update_attributes({:count => c.count+1}) # Increment the number of calculations performed for these attributes
     end
     sleep(0.3) ## Inserted this so that the buttons can be seen in their disabled state, otherwise the ajax call is too fast for it to be seen.
     respond_to do |format|   
